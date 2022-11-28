a94fd27e014119dd32fedaebc37a85324d7339e8
Happyybear <wang1993@>
2022-11-28 22:27:53 +0800

    security

---
+++ common/src/main/java/com/sem/semprovider/config/RedisConfig.java
@@ -0,0 +1,29 @@
+package com.sem.semprovider.config;
+
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+//@Configuration
+public class RedisConfig {
+//    @Bean
+    @SuppressWarnings(value = {"rawtypes"})
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory connectionFactory){
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory);
+
+        FastJsonRedisSerializer serializer = new FastJsonRedisSerializer<>(Object.class);
+
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setKeySerializer(serializer);
+
+        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
+        redisTemplate.setHashValueSerializer(serializer);
+
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+}
--- sem-security/src/main/java/com/sem/security/config/SecurityConfig.java
+++
@@ -1,6 +0,0 @@
-package com.sem.security.config;
-
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-}
